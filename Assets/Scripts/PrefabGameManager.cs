8549c2f19846fe18ff0e86908e221c44901814d8
hideyoru0 <hideyoru0@gmail.com>
2024-12-17 13:45:54 +0900

    [stage,finish]

--- Assets/Scripts/PrefabGameManager.cs
+++ Assets/Scripts/PrefabGameManager.cs
@@ -19,12 +19,12 @@ public class PrefabGameManager : MonoBehaviour
         for(int i = 0; i < 100; i++)
         {
             int randomCount = Random.Range(0, 3000) % 3;
-            var position = new Vector3(Random.Range(100.0f, 50.0f), Random.Range(100.0f, 50.0f), Random.Range(100.0f, 50.0f));
+            var position = new Vector3(Random.Range(-100.0f, 1.0f), Random.Range(100.0f, 1.0f), Random.Range(100.0f, 1.0f));
             Instantiate(mapList[randomCount], position, Quaternion.identity);
         }
 
-        // 마지막 골라인 맵을 생성하고 z축으로 15.0f만큼 떨어진 곳에 생성한다.
-        Instantiate(goalMap, new Vector3(0, 0, 15.0f), Quaternion.identity);
+        // 마지막 골라인 맵을 생성하고 y축 150.0f, z축으로 50.0f만큼 떨어진 곳에 생성한다.
+        Instantiate(goalMap, new Vector3(0, 10.0f, 10.0f), Quaternion.identity);
 
         //플레이어를 spawnPoint에 생성한다.
         Instantiate(player, spawnPoint);
